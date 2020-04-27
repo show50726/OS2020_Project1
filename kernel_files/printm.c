9c1610810431ae821ccf2b58584dc984cf37a0a6
王小明 <k40526@hotmail.com>
2020-04-27 18:36:20 +0800

    update printm.c

--- kernel_files/printm.c
+++ kernel_files/printm.c
@@ -1,6 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/linkage.h>
-SYSCALL_DEFINE3(sys_printm, int pid, long start, long end){
+SYSCALL_DEFINE3(sys_printm, int, pid, long, start, long, end){
 	static const long ns = 1000000000;
     	printk(KERN_INFO "[Project1] %d %ld.%09ld %ld.%09ld\n", pid, start/ns, start%ns, end/ns, end%ns);
     	return 0;
